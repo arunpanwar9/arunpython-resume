a61ab6f4f66366e495d992f6a9642daf72a40cba
arun <arunpanwar9>
2022-11-26 20:29:33 +0530

    make it better

--- resume_project/settings.py
+++ resume_project/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+#SECRET_KEY = os.environ['SECRET_KEY']
 
-#key = '#django-insecure-2*6k+zk0ltod*vw-=ni84+_*$kac!)tk(^r&)i3cyy@q0*m7ib'
+SECRET_KEY= 'django-insecure-2*6k+zk0ltod*vw-=ni84+_*$kac!)tk(^r&)i3cyy@q0*m7ib'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -33,9 +33,7 @@ ALLOWED_HOSTS = ['arunpython-resume.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'material',
-    'material.admin',
-    #'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -124,11 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, "static")
+#]
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "static"

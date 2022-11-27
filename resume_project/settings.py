7c95e59bae38cf976d19e65d47ce55351848ce21
arun <arunpanwar9>
2022-11-27 16:52:02 +0530

    make it better

--- resume_project/settings.py
+++ resume_project/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 
-SECRET_KEY= 'django-insecure-2*6k+zk0ltod*vw-=ni84+_*$kac!)tk(^r&)i3cyy@q0*m7ib'
+#SECRET_KEY= 'django-insecure-2*6k+zk0ltod*vw-=ni84+_*$kac!)tk(^r&)i3cyy@q0*m7ib'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

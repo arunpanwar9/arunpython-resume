96db914e51f3770c2c69afd2daad6337ec34ebef
arun <arunpanwar9>
2022-12-01 13:43:27 +0530

    update 3.9

--- resume_project/settings.py
+++ resume_project/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
     #os.path.join(BASE_DIR, "static")
 #]
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "static"
+#STATIC_URL = '/static/'
+#STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
